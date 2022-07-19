64dfe1bafa166a3e95e054edc65da8c360bfc780
Karl Savinskiy <karlsavinskiy@gmail.com>
2022-07-19 03:17:17 -0700

    commit tree 7adaba10b72818eb2949e78dc48bfa412e193984

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(somevalue) {
+    if (somevalue > 42) {
+        return somevalue - 42
+    } else if (somevalue < 42) {
+        return 42 - somevalue 
+    }
+}
+function distanceFromHqInFeet(somevalue) {
+    return distanceFromHqInBlocks(somevalue)*264;
+}
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination){
+        return (start-destination)*264
+    } else if ( start < destination) {
+        return (destination - start)*264
+    }
+}
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) < 400) {
+        return 0
+    } else if (distanceTravelledInFeet(start, destination) < 2000) {
+        return  (distanceTravelledInFeet(start, destination) - 400) * 0.02 
+    } else if (distanceTravelledInFeet(start, destination) < 2500) {
+        return 25 
+    } else {
+        return 'cannot travel that far'
+    }
+}
